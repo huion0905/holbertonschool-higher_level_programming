3996d00f2dd1f03e069ba211d2194ea24c14e9ca
Your Name <you@example.com>
2023-11-12 20:30:28 -0800

    programa que imprima el resultado de la suma de todos los argumentos

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    from sys import argv
+
+    result = 0
+
+    for argument in argv[1:]:
+        if (argument < '0'):
+            argument = int(argument)
+        result += int(argument)
+
+    print("{:d}".format(result))
