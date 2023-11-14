312a63a5ed5d53c981957dba406dbed870561085
Your Name <you@example.com>
2023-11-14 14:46:05 -0800

    función que devuelve un nuevo diccionario con todos los valores multiplicados por 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = dict(map(lambda kv: kv[0], kv[1] * 2), a_dictionary.items())
+    new_dict = dict(map(lambda kv: (kv[0], kv[1] * 2), a_dictionary.items()))
     return (new_dict)
